aed4811bc29ec6d7ea572d671e9147b6eec3beb6
hrithikaaaa <hrithvl03@gmail.com>
2026-04-30 14:00:10 +0530

    fix cors url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // ─── Middleware ───────────────────────────────────────────────────────────────
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:3000', 'https://folio-frontend-zeta.vercel.app'],
   credentials: true,
 }));
 app.use(express.json());
